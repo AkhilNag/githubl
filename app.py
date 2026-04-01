571850462954b478fbb30ddf52b2ada619f42738
AkhilNag <akhil.macherla@gmail.com>
2026-04-01 21:23:14 +0530

    sql query added

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ print('Hello')
 def gittest():
     print("This is a git test function.")
 gittest()
+
+CREATE TABLE [dbo].[TableCopy](
+	[TableID] [int] IDENTITY,
+    [TableName] varchar(200) NOT NULL
+)
+
+INSERT INTO [dbo].[TableCopy] VALUES('[SalesLT].[Product]')
+INSERT INTO [dbo].[TableCopy] VALUES('[SalesLT].[ProductCategory]')
+INSERT INTO [dbo].[TableCopy] VALUES('[SalesLT].[ProductModel]')
+
+SELECT [TableName] FROM [dbo].[TableCopy]
